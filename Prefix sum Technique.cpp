cbbef07eb7fa2afac865e6c3b14abe1596f7b020
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2022-10-27 22:21:38 +0600

    Add files via upload

--- Prefix sum Technique.cpp
+++ Prefix sum Technique.cpp
@@ -2,17 +2,41 @@
 using namespace std;
 int main()
 {
-    int a[10]={1,2,3,4,5,6,7,8,9,10};
-    int prefix[10];
+    /* prefix sum technique
+    a[5]={1,2,3,4,5};
+    prefix[5]={1,3,6,10,15};
+    if l=1 and r=3
+       then output 9
+    if l=0 and l=4
+    output 10
+     */
+    int n;
+    cin>>n;
+    int a[n];
+    int prefix[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
     prefix[0]=a[0];
+    /*
+    By this loop,we will accomplish a task so that each element of prefix array is
+    equal to the total sum of previous all element.
+    */
     for(int i=1;i<10;i++)
     {
         prefix[i]=prefix[i-1]+a[i];
     }
-    int q=5;
+    cout<<"Enter the number of Query=";
+    int q;
+    cin>>q;
+    /*
+    By this loop, we will determine summation between the range l and r.
+    */
     for(int i=0;i<q;i++)
     {
        int l,r;
+       cout<<"Enter Range="<<endl;
        cin>>l>>r;
        if(l==0)
        {
